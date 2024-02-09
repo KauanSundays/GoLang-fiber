d80cc956c009f8ea6e615f51f5c28643b6ab9e2d
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-02-09 17:59:10 -0300

    basic config

--- maing.go
+++ maing.go
@@ -1,8 +1,13 @@
 package main
 
-// Hello returns a greeting for the named person.
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+import()
+
+func main(){
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app := fiber.New()
+	
 }
