7d6d8e0df38a5e2aa1fd8b6b864fee9adb635e04
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-02-09 18:18:01 -0300

    imports

--- maing.go
+++ maing.go
@@ -1,6 +1,14 @@
 package main
 
-import()
+import(
+	"github.com/gofiber/fiber/v3"
+	"gorm.io/gorm"
+	"https://github.com/joho/godotenv"
+)
+
+type Reposutory struct{
+	DB
+}
 
 func main(){
 	err := godotenv.Load(".env") //  Load  .env file
