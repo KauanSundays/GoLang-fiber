9dc8860433bf1fa1f8f6ba97af26b4b9fd46e649
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-02-15 19:35:16 -0300

    wip

--- storage/postgres.go
+++ storage/postgres.go
@@ -18,5 +18,8 @@ type Config struct {
 func NewConnection(config *Config)(*gorm.DB, error) {
 	dsn : fmt.Sprintf(
 		"host=%s  port=%s user=%s password=%s dbname=%s sslmode=%s", 
+		config.Host, config.Port, config.User, config.Password, config.DBName, config.SSLMode
 	)
+	gorm.Open(postgres,Open(dsn), &gorm.Config{})
+	
 }
